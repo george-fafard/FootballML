75d844d2fbc746ef706d7565e71209b02f4a2201
rafarrel <rfarrel2@uvm.edu>
2021-05-06 16:30:19 -0400

    Added example to ensemble classifier code and notebook

--- FootballML/Classifiers/Ensemble/ensemble_classifier.py
+++ FootballML/Classifiers/Ensemble/ensemble_classifier.py
@@ -1,1 +1,9 @@
-# This will be where the actual code for the ensemble classifier goes
+"""
+    This will be where the actual code for the ensemble classifier goes.
+"""
+# THIS IS AN EXAMPLE. WE CAN REMOVE IT WHEN WE START WORKING ON THIS
+from FootballML.Dataset.cleaned_data import read_game_data_from_files 
+
+# Get test data
+def test_data():
+    return read_game_data_from_files(2005, 2010) 
