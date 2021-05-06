6d484b67c5c144167d2db88b7f2b7ea31110a9f8
rafarrel <rfarrel2@uvm.edu>
2021-05-06 15:53:48 -0400

    Fixed root path error for DATA_PATH

--- FootballML/__init__.py
+++ FootballML/__init__.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 
 # Root directory
-ROOT_DIRECTORY = str(pathlib.Path(os.path.dirname(os.path.abspath(''))))
+ROOT_DIRECTORY = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))))
 
 # Set data path
-DATA_PATH = str(pathlib.Path(ROOT_DIRECTORY + '/FootballML/Dataset/Loaded_Raw_Data/'))
+DATA_PATH = str(pathlib.Path(ROOT_DIRECTORY + '/Dataset/Loaded_Raw_Data/'))
